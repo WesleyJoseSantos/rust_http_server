b856f771bc85d008d3d5904c8adc331fe5a1dc6c
Wesley Santos <binary_quantum@outlook.com>
2022-12-01 20:23:13 -0300

    feat: first commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("192.168.27.182:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("C:\\Users\\BINAR\\OneDrive\\Documentos\\GitHub\\training\\rust\\http_server\\src\\statusLed.txt").unwrap();
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
+
+        println!("{}\n", contents);
+    } else {
+        // some other request
+    }
+}
