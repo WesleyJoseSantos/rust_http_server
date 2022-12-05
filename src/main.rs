d66b30a5505ff909d64f838ec567d7696fe1a8da
Wesley Santos <BINARY_QUANTUM@outlook.com>
2022-12-05 11:38:46 -0300

    feat: implement web server

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use std::{
+    str,
     fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
+    io::{prelude::*},
+    net::{TcpListener, TcpStream}, time::Duration,
 };
 
-const SERVER_URL: &'static str = "192.168.27.215:7878";
-const COMMAND_FILENAME: &'static str = "status_led.txt";
+const SERVER_URL: &'static str = "192.168.50.215:7878";
+const INDEX_FILENAME: &'static str = "index.html";
+const STATUS_LED_FILENAME: &'static str = "status_led.txt";
 
 fn main() {
     let listener = TcpListener::bind(SERVER_URL).unwrap();
 
-    println!("{}", std::env::current_dir().unwrap().display());
+    println!("Application is running at http://{}", SERVER_URL);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -20,20 +22,43 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let mut data = String::new();
+    stream.set_read_timeout(Some(Duration::from_millis(150))).unwrap();
+    stream.read_to_string(&mut data).unwrap_or(0);
+    let mut lines = data.lines();
+
+    let request_line = lines.next().unwrap_or("");
 
     if request_line == "GET / HTTP/1.1" {
         let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string(COMMAND_FILENAME).unwrap();
+        let contents = fs::read_to_string(INDEX_FILENAME).unwrap();
         let length = contents.len();
 
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
         stream.write_all(response.as_bytes()).unwrap();
-    } else {
-        // some other request
+    } else if request_line == "GET /status_led HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string(STATUS_LED_FILENAME).unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    } else if request_line == "POST /status_led HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = String::from("OK");
+        let length = contents.len();
+        let mut body : &str = "teste";
+
+        for line in lines {
+            body = line;
+        }
+
+        fs::write(STATUS_LED_FILENAME, body).unwrap();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
     }
 }
